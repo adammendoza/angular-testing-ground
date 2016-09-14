ce589e50eedc82d0f233b303aca354bccef85d2a
blacksonic <soos.gabor86@gmail.com>
2016-09-14 21:30:53 +0200

    add mocking tests

---
+++ src/app/testing-series-services/car.service.spec.ts
@@ -0,0 +1,79 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { Injectable } from '@angular/core';
+import { Engine } from './engine.service';
+import { Car } from './car.service';
+
+describe('Car', () => {
+  let subject: Car;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [Engine, Car]
+    });
+  });
+
+  beforeEach(inject([Car], (car: Car) => {
+    subject = car;
+  }));
+
+  it('should display name with engine', () => {
+    expect(subject.getName()).toEqual('Car with Basic engine(150 HP)');
+  });
+
+  it('should display name with engine', inject([Car], (car: Car) => {
+    expect(car.getName()).toEqual('Car with Basic engine(150 HP)');
+  }));
+});
+
+describe('CarMock', () => {
+  let subject: Car;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [Engine, Car]
+    });
+
+    spyOn(Engine.prototype, 'getHorsepower').and.returnValue(400);
+    spyOn(Engine.prototype, 'getName').and.returnValue('V8 engine');
+  });
+
+  beforeEach(inject([Car], (car: Car) => {
+    subject = car;
+  }));
+
+  it('should display name with engine', () => {
+    expect(subject.getName()).toEqual('Car with V8 engine(400 HP)');
+  });
+});
+
+@Injectable()
+class V8Engine {
+  getHorsepower() {
+    return 400;
+  }
+
+  getName() {
+    return 'V8 engine';
+  }
+}
+
+describe('CarMockDI', () => {
+  let subject: Car;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: Engine, useClass: V8Engine },
+        Car
+      ]
+    });
+  });
+
+  beforeEach(inject([Car], (car: Car) => {
+    subject = car;
+  }));
+
+  it('should display name with engine', () => {
+    expect(subject.getName()).toEqual('Car with V8 engine(400 HP)');
+  });
+});
--- src/app/testing-series-services/engine.service.spec.ts
+++ src/app/testing-series-services/engine.service.spec.ts
@@ -1,17 +1,17 @@
 import { Engine } from './engine.service';
 
 describe('Engine', () => {
-  let engine: Engine;
+  let subject: Engine;
 
   beforeEach(() => {
-    engine = new Engine();
+    subject = new Engine();
   });
 
   it('should return it\'s horsepower', () => {
-    expect(engine.getHorsepower()).toEqual(150);
+    expect(subject.getHorsepower()).toEqual(150);
   });
 
   it('should return it\'s horsepower', () => {
-    expect(engine.getName()).toEqual('Basic engine');
+    expect(subject.getName()).toEqual('Basic engine');
   });
 });
