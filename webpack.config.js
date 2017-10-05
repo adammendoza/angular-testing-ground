87945cee7183c2ab35c491e84df4f2f4a9bf7340
gsoos <soos.gabor86@gmail.com>
2017-10-05 22:12:04 +0200

    upgrade to webpack 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,26 @@
 'use strict';
+
 const path = require('path');
+const webpack =require('webpack');
 
 module.exports = {
   resolve: {
-    root: [ path.join(__dirname, 'src') ],
-    extensions: ['', '.ts', '.js']
+    extensions: ['.ts', '.js', '.json']
   },
 
   module: {
-    loaders: [
+    rules: [
       { test: /\.ts$/, loaders: ['awesome-typescript-loader', 'angular2-template-loader'] },
       { test: /\.html$/, loader: 'raw-loader' }
     ]
   },
+
+  plugins: [
+    new webpack.ContextReplacementPlugin(
+      /angular(\\|\/)core(\\|\/)@angular/,
+      path.join(process.cwd(), 'src')
+    )
+  ],
 
   devtool: 'inline-cheap-source-map'
 };
